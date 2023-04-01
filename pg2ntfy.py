43c52de8143d101f5f844f8bc2b45ae7c942a921
Georg Sauthoff <mail@gms.tf>
2023-04-01 18:49:49 +0200

    add Postgres -> ntfy service

---
+++ pg2ntfy.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+
+# pg2ntfy - forward Postgresql notifications to a ntfy.sh instance
+#
+# SPDX-FileCopyrightText: © 2023 Georg Sauthoff <mail@gms.tf>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+import configargparse
+import httpx
+import psycopg2
+import select
+import signal
+import sys
+import systemd.daemon
+
+prefix = '/usr'
+prog   = 'pg2ntfy'
+
+def parse_args():
+    p = configargparse.ArgParser(
+            auto_env_var_prefix=prog + '_',
+            default_config_files=[f'{prefix}/lib/{prog}/config.ini',
+                f'/etc/{prog}.ini', f'~/.config/{prog}.ini'])
+    p.add('-c', '--config', is_config_file=True,
+          help='config file')
+    p.add('--channel', required=True,
+          help='Postgres NOTIFY channel to LISTEN to and forward')
+    p.add('--db', '-d', default='dbname=metricsdb',
+          help='PostgreSQL metrics database URL (default: %(default)s)')
+    p.add('--ntfy', default='https://ntfy.sh',
+          help='ntfy server (default: %(default)s)')
+    p.add('--systemd', action='store_true',
+          help='notify systemd during startup')
+    p.add('--topic', required=True, help='Ntfy topic to post to')
+    args = p.parse_args()
+    return args
+
+
+def on_sigterm(sig, frm):
+    raise KeyboardInterrupt()
+
+
+def mainP():
+    signal.signal(signal.SIGTERM, on_sigterm)
+    args = parse_args()
+
+    db = psycopg2.connect(args.db)
+    db.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    c = db.cursor()
+
+    c.execute(f'LISTEN {args.channel}')
+
+    if args.systemd:
+        systemd.daemon.notify('READY=1')
+
+    p = select.poll()
+    p.register(db, select.POLLIN)
+
+    while p.poll():
+        db.poll()
+        while db.notifies:
+            x = db.notifies.pop(0)
+            httpx.post(f'https://ntfy.sh/{args.topic}', content=x.payload)
+
+
+def main():
+    try:
+        return mainP()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    sys.exit(main())
